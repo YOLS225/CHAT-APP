83d70d04183ee055bd385882239bf17e6684574b
YOANN LASM <marc.lasm@st2i.net>
2025-11-13 11:25:58 +0000

    add correction

--- src/app/features/rooms/components/index.tsx
+++
@@ -1,8 +0,0 @@
-// Room list components
-export { CardList } from './room-list';
-export type { RoomItemProps } from './room-list';
-
-// Room creation components
-export { default as RoomStepper } from './room-stepper';
-export { SelectUsersForm, RoomConfigForm, RoomConfirmationForm } from './room-forms';
-export type { RoomCreationContextType, UserProps } from './room-stepper';
